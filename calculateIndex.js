7409f9aaff5d2de6755205759cae2c6493639a8e
Tom Février <tomfevrier@gmail.com>
2017-04-17 14:07:30 +0200

    Delete calculateIndex.js

--- calculateIndex.js
+++
@@ -1,7 +0,0 @@
-var tab = [];
-
-for (var i = 0; i < dataCulture.length; i++) {
-    tab.push(((parseFloat(dataCoutDeLaVie[i].index) + parseFloat(dataTransport[i].index) + parseFloat(dataCadreDeVie[i].index) + parseFloat(dataSorties[i].index) + parseFloat(dataCulture[i].index))/5).toFixed(3));
-}
-
-initMap();
